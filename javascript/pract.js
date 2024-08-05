6f06d26c41cb98c6c77b2a112e87e9e22bdce12d
Sri Gowri Devi <gowri19012003@gmail.com>
2024-08-05 18:40:08 +0530

    Add files via upload

---
+++ javascript/pract.js
@@ -0,0 +1,50 @@
+    //In JavaScript, console. log is a built-in function that allows you to output messages or values to the console. It is commonly used for debugging and understanding what is happening in your code.
+    /*console.log("gowri bharathi");
+    alert("mrsevp");
+    console.log({"fname":"gowri","age":23 ,"hometown":"tenkasi"});
+    console.table([84,94,44,565]);
+    console.table([84,94,44,565]);
+    console.error("something wrong");
+    console.warn("something went wrong in your code");
+    console.clear();  //console was cleared;
+    console.time("timer");
+    for(i=0;i<50;i++){
+    console.log(i);
+    }
+    console.timeEnd("timer");
+
+  / value assign
+var g = 65;
+var h = 75;
+console.log(g+h);
+
+  if(true){
+      var msg = "say hello";
+      let msg ="say hello";
+     const msg="say hello";
+      console.log(msg);
+  }
+  console.log(msg);
+  variable declaration
+  var => re assign
+  let => we can assign  the value only one time, can not re assign and  for sustainablity.
+
+       var a= 60; 
+       console.log(a);
+when value assign in variable you cant resign or chnge the value however,
+when  you assign the value in object you can change the value in const
+   let a= 10;
+   console.log(a);
+   a= 40;
+   console.log(a);
+  const student = {'name':'dev','age':23};
+  console.table(student);
+  student.name = 'Bharathi';
+  console.log(student.name);
+  
+   var let const are keywords thats allows to declare the variable ..
+   the scope of variable tells us where can we access inisde our code the varible and where we can't..
+   majaor difference based on access..
+   var -> global variable
+   let ,const -> private
+ */
